b904f7a8d25457606ad3e4337411b18f0b22ff0b
Dhaivat Pandit <dhaivat.pandit@sojern.com>
2017-05-22 14:58:48 -0700

    custom: 9.0.2

--- pip/__init__.py
+++ pip/__init__.py
@@ -61,7 +61,7 @@ import pip.cmdoptions
 cmdoptions = pip.cmdoptions
 
 # The version as used in the setup.py and the docs conf.py
-__version__ = "10.0.0.dev0"
+__version__ = "9.0.2"
 
 
 logger = logging.getLogger(__name__)
